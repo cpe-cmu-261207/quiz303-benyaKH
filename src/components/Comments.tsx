c421cdadefceb81a510e80e410e492e83bc2a618
Kaofang <benyapa_kh@cmu.ac.th>
2021-09-09 15:22:39 +0700

    ;(

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -14,7 +14,10 @@ const Comments = (com : CommentType) => {
         <p className="font-semibold">{com.username}</p>
         <p>{com.commentText}</p>
         {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
-        
+        <div className='flex items-center'>
+        <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+        <p className='text-gray-500'>{com.likeNum} คน</p>
+    </div>
       </div>
     </div>
     {com.replies.map(x => <Rep username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies}/>)}
--- src/components/Replies.tsx
+++ src/components/Replies.tsx
@@ -1,5 +1,5 @@
 import { CommentType, comments } from "../data/comments";
-const Rep = (m : CommentType) => {
+const Rep = (m: CommentType) => {
     return (
 
         <div className="flex p-2 items-start space-x-2 pl-14">
@@ -7,6 +7,10 @@ const Rep = (m : CommentType) => {
             <div className="bg-gray-200 rounded-lg p-2">
                 <p className="font-semibold">{m.username}</p>
                 <p>{m.commentText}</p>
+            </div>
+            <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-500'>{m.likeNum} คน</p>
             </div>
         </div>
     )
